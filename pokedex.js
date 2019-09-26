45f1184a8a54cb7f7f4bda29fe35f82a1c793c0b
darioviada <darioviada@gmail.com>
2019-09-26 19:25:27 -0300

    subida

---
+++ pokedex.js
@@ -0,0 +1,26 @@
+var pokedex = new XMLHttpRequest();
+
+pokedex.onreadystatechange = function() {
+    if(this.readyState === 4 && this.status === 200){
+        var respuesta = JSON.parse(pokedex.responseText);
+        var habilidades =[];
+        console.log(respuesta);
+        //document.body.innerHTML = respuesta.abilities[0].ability.name;
+        document.getElementById("titulo").innerHTML = respuesta.name;
+        document.getElementById("foto_titulo").innerHTML = respuesta.name;
+        document.getElementById("imagen_frente").src = respuesta.sprites.front_default;
+        document.getElementById("imagen_dorso").src = respuesta.sprites.back_default;
+        document.getElementById("is_default").innerHTML = "Default Character: " + respuesta.is_default;
+        document.getElementById("base_experience").innerHTML = "Base Experience: " + respuesta.base_experience;
+        document.getElementById("height").innerHTML = "Height: " + respuesta.height;
+        document.getElementById("weight").innerHTML = "Weigth: " + respuesta.weight;
+        for(var i=0; i < respuesta.abilities.length; i++){
+            habilidades.push(respuesta.abilities[i].ability.name);
+        }
+              
+        document.getElementById("abilities").innerHTML = "Abilities: " + habilidades; 
+    }
+};
+
+pokedex.open("GET" , "https://pokeapi.co/api/v2/pokemon/pikachu/");
+pokedex.send();
